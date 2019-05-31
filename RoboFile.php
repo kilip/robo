645ed78a40e6c1813700d9137d2cc09d9dca079d
Anthonius Munthi <me@itstoni.com>
2019-05-31 12:01:53 +0800

    Robo release 1.4.9-lurker

--- RoboFile.php
+++ RoboFile.php
@@ -499,7 +499,9 @@ class RoboFile extends \Robo\Tasks
         $this->checkPharReadonly();
 
         $version = shell_exec('git describe --tags `git rev-list --tags --max-count=1`');
-        $version = trim($version).'-lurker';
+        if(false === strpos($version,'lurker')){
+            $version = trim($version).'-lurker';
+        }
 
         $this->yell("Releasing Robo $version");
 
